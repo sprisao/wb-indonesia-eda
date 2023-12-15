ef36c3027e28b307b422723d30a0873b0eec94d1
brucedev <whdtmek@gmail.com>
2023-12-15 10:09:42 +0900

    excel 파일 병합하여 csv 파일로 추출

--- src/merge_excel.py
+++ src/merge_excel.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 
 def merge_excel_to_csv(input_folder, output_folder, output_file):
+    # Check if the input directory exists
+    if not os.path.isdir(input_folder):
+        raise FileNotFoundError(f"Input directory '{input_folder}' does not exist.")
+
     # Excel 파일 목록을 불러온다
     excel_files = [f for f in os.listdir(input_folder) if f.endswith(".xlsx")]
 
@@ -13,17 +17,20 @@ def merge_excel_to_csv(input_folder, output_folder, output_file):
         df = pd.read_excel(file_path)
         dataframes.append(df)
 
-
     # 데이터프레임을 하나로 합친다
     merged_df = pd.concat(dataframes)
+
+    # Check if the output directory exists, if not create it
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
 
     # 합친 데이터프레임을 CSV 파일로 저장한다
     output_path = os.path.join(output_folder, output_file)
     merged_df.to_csv(output_path, index=False)
 
 
-input_folder = "data/origin"
-output_folder = "data/csv"
+input_folder = "../data/original"
+output_folder = "../data/csv"
 output_file = "merged.csv"
 
 merge_excel_to_csv(input_folder, output_folder, output_file)
